28af3b02efb3eeae5f14fb69924836a128ab308d
antoineedwards <edwardsantoine333@gmail.com>
2024-08-02 17:43:55 -0500

    6:43 fri

--- inventory/app/page.js
+++ inventory/app/page.js
@@ -2,12 +2,14 @@
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { firestore } from '@/firebase'
-import { Modal, Box, Stack, Typography } from "@mui/material";
+import { getFirestore } from 'firebase/firestore'
+import { Button, Modal, Box, Stack, Typography, TextField } from "@mui/material";
+import { collection, deleteDoc, doc, getDocs, query } from "firebase/firestore"
 
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState('true')
   const [itemName, setItemName] = useState('')
 
   const updateInventory = async () => {
@@ -62,16 +64,70 @@ export default function Home() {
 
   return (
     <Box width='100vw'
-    height ='100vh' 
-    display='flex' 
-    justifyContent='center'
-    alignItems='center'
-    gap={2}>
+      height='100vh'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+      gap={2}>
+        <Typography variant='h1' textAlign='center' fontSize='3em'>
+        Inventory Management
+        </Typography>
       <Modal open={open} onClose={handleClose}>
-        <Box position='absolute' top='50%'></Box>
+        <Box position='absolute' left='50%' top='50%'
+          width='400' bgcolor='white'
+          border='2px solid black'
+          boxShadow={18} p='4' padding='10px'
+          dislay={'flex'} flexDirection={'column'} gap={3}
+          sx={{
+            transform: 'translate(-50%, -50%)'}}>
+          <Typography variant='h6'>Add Item</Typography>
+          <Stack width='100%' direction='row' spacing={2}>
+            <TextField 
+            variant='outlined'
+            value={itemName}
+            fullWidth
+            onChange={(e)=>{
+              setItemName(e.target.value)
+            }}
+            ></TextField>
+            <Button
+            variant='outlined' onClick={()=>{
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}>Add</Button>
+          </Stack>
+        </Box>
       </Modal>
-      <Typography variant='h1' textAlign='center' fontSize='3em'>
-        Inventory Management</Typography>
+      <Button variant='contained' onCLick={()=>{
+        handleOpen()
+      }}>
+        Add New Item
+      </Button>
+      <Box border='1px solid #333'>
+        <Box width='800px' height='100px'
+         bgcolor='#add8e6' display='flex'
+         alignItems='center' justifyContent='center'>
+          <Typography variant='h2' color='#333'>
+            Inventory Items
+          </Typography>
+        </Box>
+      </Box>
+      <Stack width='800px' height='300px' spacing={2} overflow='auto'>
+        {
+          inventory.map(({name, quantity})=>{
+            <Box key={name} width='100%' minHeight='150px'
+            display='flex' alignItems='center'
+            justifyContent='center'
+            bgColor='#f0f0f0'
+            padding='5'>
+              <Typography variant='h3'>{name}</Typography>
+                            
+            </Box>
+          })
+        }
+      </Stack>
     </Box>
   )
 }
